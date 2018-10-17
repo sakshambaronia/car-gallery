dd9f98b07c875297e105375682928b15fc2de41b
Saksham Baronia <baronia.saksham@gmail.com>
2018-10-18 04:44:41 +0530

    Security

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 $server = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b7d9aeef5637e3";
-$password = "81cea61c";
+$password = "*********";
 $db = "heroku_821487f24eaafd3";
 
 $config = array(
